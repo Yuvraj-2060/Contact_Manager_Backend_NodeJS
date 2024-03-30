757488b2eedf58de20ad91863699ae6bcb5aa8ea
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-03-30 17:01:59 +0530

    Added userRoutes- register, login, current.

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post("/register", (req, res) =>{
+    res.json({ message: "Register User"});
+});
+
+router.post("/login", (req,res) =>{
+    res.json({ message: "Login User"})
+});
+
+router.get("/current", (req,res) =>{
+    res.json({ message: "Current User Information."})
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDb = require("./config/dbConnection")
 const coursesRouter = require('./routes/contactRoutes');
+const usersRouter = require('./routes/userRoutes')
 const errorHandler = require('./middleware/errorhandler');
 const dotenv = require("dotenv").config();
 
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/v1/contacts", coursesRouter);
+app.use("/api/v1/users", usersRouter);
 app.use(errorHandler);
 
 app.listen(port, ()=>{
