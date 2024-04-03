c3e0db18c5d2093cec85b397b3881b1d6a090f89
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-04-03 23:46:12 +0530

    Made changes in contactRoutes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,16 +1,16 @@
 const express = require("express")
-const {getContacts,createContact,getContact,updateContact,deleteContact} = require("../controllers/contactController.js")
+const {getContacts,createContact,getContact,updateContact,deleteContact} = require("../controllers/contactController.js");
+const validateToken = require("../middleware/validateTokenHandler.js");
 //const {route} = require("express/lib/application")
 const router = express.Router();
 
-router.get("/", getContacts);
 
+router.use(validateToken);
+router.route("/").get( getContacts).post(createContact);
+//   OR you can make separate call. just like below
+// router.post("/", createContact);
 router.get("/:id", getContact);
-
-router.post("/", createContact);
-
 router.put("/:id", updateContact);
-
 router.delete("/:id", deleteContact);
 
 
